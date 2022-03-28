ddb934d98fd80f49585439ab7ec556320aa4223c
snxoyaze <82806256+snxoyaze@users.noreply.github.com>
2022-03-28 18:26:56 +0400

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ db = BotDatabase('database.db')
 
 
 def start_command(update, context):
-    message = 'Hey! I can help notify everyone 📢 in the group when someone needs them. \
+    message = '
+ Salam LionyNetwork Tag Botuna Xoş Gelmisiz Hey! I can help notify everyone 📢 in the group when someone needs them. \
 Everyone who wishes to receive mentions needs to /in to opt-in. All opted-in users can then be mentioned using /all'
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
